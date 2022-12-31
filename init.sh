90cea9834458055940961832ed2528d8e7b914c1
Coltrane Willsey <cwillsey06@gmail.com>
2022-12-31 15:35:24 -0700

    Remove redundant checks and awaits in init.sh

--- init.sh
+++ init.sh
@@ -11,54 +11,17 @@
 export CONFIG_HOME="${HOME}/.config/hypr"
 export ACTIVE_THEME="${CONFIG_HOME}/themes/active"
 
-# Include cask-print lib
-source ${CONFIG_HOME}/bin/cask_print.sh
-
 # Array of programs to kill
 programs=("swww-daemon" "xsettingsd" "waybar" "mako")
 
-
-function kill_all {
-	for prog in ${programs[@]}; do
-		pkill -9 $prog
-	done
-}
-
-# Start process
-cask_print --progress "Starting reinitialization process..."
-
 # Kill all relevant programs
-kill_all &
-
-# Await closure
 for prog in ${programs[@]}; do
-	if [ -z $(pidof $prog) ]; then
-		break;
-	else
-		echo "Awaiting process termination..."
-	fi
+	pkill -9 $prog
 done
-
-cask_print --progress "All relevant processes terminated."
 
 # Reinitialize
 ${CONFIG_HOME}/bin/swww_init.sh &
 xsettingsd --config ${ACTIVE_THEME}/xsettingsd          &> /dev/null &
 waybar     --config ${ACTIVE_THEME}/waybar/config.jsonc &> /dev/null &
 mako       --config ${ACTIVE_THEME}/makorc              &> /dev/null &
-
-
-# Await init
-for prog in ${programs[@]}; do
-	if [ -z $(pidof $prog) ]; then
-		echo "Awaiting process initializtion..."
-	else
-		break
-	fi
-done
-
-cask_print --progress "All relevant processes reinitialized."
-
-# Finish
-cask_print --info "Done!"
 
